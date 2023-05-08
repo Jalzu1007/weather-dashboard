fa65c17d1e506b9a5c09eeebd1a5f5546e617eeb
Jose Alzuri <jalzu1007@gmail.com>
2023-05-08 12:47:22 -0400

    Added forecast data for five day week to JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
             //grabbing the values from the data object and assigning them to HTML ids
             $(".forecast-temp").text("Temperature (F): " + tempF)
             $(".forecast-hws").text("Humidity: " + data.main.humidity)
-            $(".forecast-ws").text("Wind Speed: " + data.wind.speed)
+            $(".forecast-ws").text("Wind: " + data.wind.speed)
         }
     });
   
@@ -64,29 +64,55 @@ $(document).ready(function () {
                 return (formattedTime);
             };
 
-             //url for the icons
-             let iconURL = "https://openweathermap.org/img/w/";
+            //url for the icons
+            let iconURL = "https://openweathermap.org/img/w/";
 
-             //grabbing city name value and assigning them to the HTML
-             $(".city-name").text(searchValue + " " + timeConverter(today));
+            //grabbing city name value and assigning them to the HTML
+            $(".city-name").text(searchValue + " " + timeConverter(today));
 
-             //grabbing icon value and assigning them to the HTML
-             $("#icon-main").attr("src", iconURL + data2.daily[0].weather[0].icon + ".png")
-             $("#icon-one").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
-             $("#icon-two").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
-             $("#icon-three").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
-             $("#icon-four").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
-             $("#icon-five").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
+            //grabbing icon value and assigning them to the HTML
+            $("#icon-main").attr("src", iconURL + data2.daily[0].weather[0].icon + ".png")
+            $("#icon-one").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
+            $("#icon-two").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
+            $("#icon-three").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
+            $("#icon-four").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
+            $("#icon-five").attr("src", iconURL + data2.daily[1].weather[0].icon + ".png")
 
-             //grabbing the forecast date and assigning them to the HTML
-             $(".forecast-ones").text(timeConverter(dayOne));
-             $(".forecast-twos").text(timeConverter(dayTwo));
-             $(".forecast-threes").text(timeConverter(dayThree));
-             $(".forecast-fours").text(timeConverter(dayFour));
-             $(".forecast-fives").text(timeConverter(dayFive));
+            //grabbing the forecast date and assigning them to the HTML
+            $(".forecast-ones").text(timeConverter(dayOne));
+            $(".forecast-twos").text(timeConverter(dayTwo));
+            $(".forecast-threes").text(timeConverter(dayThree));
+            $(".forecast-fours").text(timeConverter(dayFour));
+            $(".forecast-fives").text(timeConverter(dayFive));
 
+            //Kelvin to Fahrenheit formula
+            let tempFOne = ((data2.daily[1].temp.max - 273.15) * 1.80 + 32).toFixed(1);
+            let tempFTwo = ((data2.daily[2].temp.max - 273.15) * 1.80 + 32).toFixed(1);
+            let tempFThree = ((data2.daily[3].temp.max - 273.15) * 1.80 + 32).toFixed(1);
+            let tempFFour = ((data2.daily[4].temp.max - 273.15) * 1.80 + 32).toFixed(1);
+            let tempFFive = ((data2.daily[5].temp.max - 273.15) * 1.80 + 32).toFixed(1);
 
-        }
-    })
+            //grabbing the forecast temp and assigning them to the HTML
+            $(".forecast-temp-one").text("Temp: " + tempFOne);
+            $(".forecast-temp-two").text("Temp: " + tempFTwo);
+            $(".forecast-temp-three").text("Temp: " + tempFThree);
+            $(".forecast-temp-four").text("Temp: " + tempFFour);
+            $(".forecast-temp-five").text("Temp: " + tempFFive);
+
+            //grabbing the forecast humidity data and assigning them to the HTML
+            $(".forecast-hum-one").text("Humidity: " + data2.daily[1].humidity);
+            $(".forecast-hum-two").text("Humidity: " + data2.daily[2].humidity);
+            $(".forecast-hum-three").text("Humidity: " + data2.daily[3].humidity);
+            $(".forecast-hum-four").text("Humidity: " + data2.daily[4].humidity);
+            $(".forecast-hum-five").text("Humidity: " + data2.daily[5].humidity);
+
+            //grabbing the forecast wind speed data and assigning them to the HTML
+            $(".forecast-ws-one").text("Wind: " + data2.daily[1].wind.speed);
+            $(".forecast-ws-two").text("Wind: " + data2.daily[2].wind.speed);
+            $(".forecast-ws-three").text("Wind: " + data2.daily[3].wind.speed);
+            $(".forecast-ws-four").text("Wind: " + data2.daily[4].wind.speed);
+            $(".forecast-ws-five").text("Wind: " + data2.daily[5].wind.speed);
+            }
+        })
     }
 })
